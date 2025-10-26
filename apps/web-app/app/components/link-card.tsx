f4cd7d0404480a6f3ffcbc0ef1da3fc8ec166b68
Filip <>
2025-10-26 04:32:22 +0100

    feat(web-app/link-card): redesign component

--- apps/web-app/app/components/link-card.tsx
+++ apps/web-app/app/components/link-card.tsx
@@ -2,10 +2,9 @@ import {
   Button,
   ButtonGroup,
   Card,
-  CardBody,
+  CardFooter,
   CardHeader,
   Chip,
-  Link,
 } from "@heroui/react";
 import { Edit, ExternalLink, Trash2 } from "lucide-react";
 import type { Link as LinkItem } from "../data/types";
@@ -20,23 +19,13 @@ export function LinkCard({ link, onEdit, onDelete }: LinkCardProps) {
   const linkDomain = new URL(link.url).hostname;
 
   return (
-    <Card className="flex flex-col gap-1">
-      <CardHeader className="flex gap-2 items-start">
-        <div className="w-full flex flex-col gap-4">
-          <div>
-            <h3 className="text-lg font-semibold">{link.name}</h3>
-            <p className="text-sm text-default-600">{linkDomain}</p>
-          </div>
-          <Link
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm flex gap-1"
-          >
-            Open in new tab
-            <ExternalLink size={14} />
-          </Link>
+    <Card className="justify-between">
+      <CardHeader className="flex items-start flex-1 justify-between">
+        <div>
+          <p className="text-sm text-default-600">{linkDomain}</p>
+          <h3 className="text-lg font-semibold">{link.name}</h3>
         </div>
+
         <ButtonGroup size="sm" variant="flat">
           <Button isIconOnly onPress={() => onEdit(link)}>
             <Edit size={16} />
@@ -46,19 +35,32 @@ export function LinkCard({ link, onEdit, onDelete }: LinkCardProps) {
           </Button>
         </ButtonGroup>
       </CardHeader>
-      <CardBody className="pt-0">
-        <div className="flex flex-wrap gap-2">
-          {link.tags.map((tag) => (
-            <Chip
-              key={tag.id}
-              size="sm"
-              style={{ backgroundColor: tag.color, color: "#fff" }}
-            >
-              {tag.name}
-            </Chip>
-          ))}
+      <CardFooter>
+        <div className="flex flex-col gap-2 flex-1">
+          <div className="flex flex-wrap gap-2">
+            {link.tags.map((tag) => (
+              <Chip
+                key={tag.id}
+                size="sm"
+                style={{ backgroundColor: tag.color, color: "#fff" }}
+              >
+                {tag.name}
+              </Chip>
+            ))}
+          </div>
+          <Button
+            as={"a"}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full"
+            color="default"
+          >
+            Open in new tab
+            <ExternalLink size={14} />
+          </Button>
         </div>
-      </CardBody>
+      </CardFooter>
     </Card>
   );
 }
