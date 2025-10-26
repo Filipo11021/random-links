1ec872dba2d20fbb72795c4e83186f4083b9cc94
Filip <>
2025-10-26 02:24:58 +0200

    feat: configure linting for all packages and apps

--- apps/web-app/app/use-type-safe-route-loader-data.tsx
+++ apps/web-app/app/use-type-safe-route-loader-data.tsx
@@ -29,7 +29,7 @@ type LoaderReturnType<Loader> = Awaited<ReturnType<IsFunction<Loader>>>;
 export function useTypeSafeRouteLoaderData<
   Id extends RouteModuleId,
   Loader extends LoaderIfExists<Id>,
-  LoaderData extends LoaderReturnType<Loader>
+  LoaderData extends LoaderReturnType<Loader>,
 >(routeId: Id): LoaderData | undefined {
   const matches = useMatches();
 
